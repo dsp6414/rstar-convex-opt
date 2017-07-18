3325a674c579548c3482a879a36aa1e2fc93cda9
t-pollington <timothy.pollington@gmail.com>
2017-07-18 14:39:51 +0100

    Tim edits to incorporate KBs comments, remove curly ineqs and double SINR expl

--- minimisePgivenSINR.py
+++ minimisePgivenSINR.py
@@ -1,21 +1,19 @@
-# Declare variables
-I = np.zeros((n,m)) # interference power matrix
-S = np.zeros((n,m)) # signal power matrix
+# Define variables
 delta = np.identity(n)
-S = G * delta # using gains matrix G
+S = G * delta
 I = G - S
 
-# Declare optimisation variable
+# Define optimisation variable
 p = cvx.Variable(n)
 
 # Define objective function
 obj = cvx.Minimize(
       cvx.sum_entries(p))
 
-# Declare constraints
-constraints =    [p >= 0,
-                  S*p-alpha*(I*p+sigma)>=0,
-                  p <= Pmax]
+# Define constraints
+constraints = [p >= 0,
+               S * p - $\alpha$ * (I * p + $\sigma$) >= 0,
+               p <= Pmax]
 
 # Solve problem
 prob = cvx.Problem(obj, constraints)
