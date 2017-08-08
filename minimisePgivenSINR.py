74531526b39e06243f8390e1929e8cc92530de9c
rpgowers <r.gowers@warwick.ac.uk>
2017-08-08 18:04:53 +0100

    Rob changes, slight tweaks

--- minimisePgivenSINR.py
+++ minimisePgivenSINR.py
@@ -1,22 +1,22 @@
 import cvxpy as cvx
 import numpy as np
 
+# Define variables
 n = 3
-k = 1
+k = 1.0
 d = np.random.rand(n,n)
 d_s = 0.5*(d+d.T)
 
 G = np.zeros((n,n))
 G = k/d_s**3.5
 
-# Define variables
 $\delta$ = np.identity(n)
 S = G * $\delta$
 I = G - S
 
 sigma = 0.1*np.ones(n)
-gamma = 1
-Pmax = 1
+gamma = 1.0
+Pmax = 1.0
 
 # Define optimisation variable
 p = cvx.Variable(n)
