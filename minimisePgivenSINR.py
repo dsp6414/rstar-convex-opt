613c7f55203cf6db4cd1ceb44d3bb887ede6ddf0
rpgowers <r.gowers@warwick.ac.uk>
2017-08-08 17:07:45 +0100

    Rob changes 08/08/17

--- minimisePgivenSINR.py
+++ minimisePgivenSINR.py
@@ -1,7 +1,22 @@
+import cvxpy as cvx
+import numpy as np
+
+n = 3
+k = 1
+d = np.random.rand(n,n)
+d_s = 0.5*(d+d.T)
+
+G = np.zeros((n,n))
+G = k/d_s**3.5
+
 # Define variables
-delta = np.identity(n)
-S = G * delta
+$\delta$ = np.identity(n)
+S = G * $\delta$
 I = G - S
+
+sigma = 0.1*np.ones(n)
+gamma = 1
+Pmax = 1
 
 # Define optimisation variable
 p = cvx.Variable(n)
@@ -12,9 +27,12 @@ obj = cvx.Minimize(
 
 # Define constraints
 constraints = [p >= 0,
-               S * p - $\alpha$ * (I * p + $\sigma$) >= 0,
+               S * p - $\gamma$ * (I * p + $\sigma$) >= 0,
                p <= Pmax]
 
-# Solve problem
+# Solve problem and print solution
 prob = cvx.Problem(obj, constraints)
 prob.solve()
+print("Solution status = %s"%(prob.status))
+print("Optimal solution = %s"%(prob.value))
+print("Power settings = %s"%(p.value))
