66fcede411913ee9fec0acfc39b390580aab3b46
rpgowers <r.gowers@warwick.ac.uk>
2017-10-25 09:21:06 +0100

    small update on python file

--- minimisePgivenSINR.py
+++ minimisePgivenSINR.py
@@ -10,12 +10,16 @@ d_s = 0.5 * (d + d.T)
 G = np.zeros((n,n))
 G = k / d_s ** 3.5
 
-$\delta$ = np.identity(n)
-S = G * $\delta$
+# $\delta$ = np.identity(n)
+δ = np.identity(n)
+# S = G * $\delta$
+S = G * δ
 I = G - S
 
-$\sigma$ = 0.1 * np.ones(n)
-$\gamma$ = 1.0
+# $\sigma$ = 0.1 * np.ones(n)
+σ = 0.1 * np.ones(n)
+# $\gamma$ = 1.0
+γ = 1.0
 Pmax = 1.0
 
 # Define optimisation variable
@@ -27,12 +31,16 @@ obj = cvx.Minimize(
 
 # Define constraints
 constraints = [p >= 0,
-               S * p - $\gamma$ * (I * p + $\sigma$) >= 0,
+               # S * p - $\gamma$ * (I * p + $\sigma$) >= 0,
+               S * p - γ * (I * p + σ) >= 0,
                p <= Pmax]
 
 # Solve problem and print solution
 prob = cvx.Problem(obj, constraints)
 prob.solve()
-print("Solution status = %s"%(prob.status))
-print("Optimal solution = %s"%(prob.value))
-print("Power settings = %s"%(p.value))
+
+print('Solution status = {0}'.format(prob.status))
+print('Optimal solution = {0:.3f}'.format(prob.value))
+if prob.status == 'optimal':
+	for j in range(n):
+		print('Power {0} = {1}'.format(j,p.value[j]))
